1ea5555edd48e234d370265ea1b03126c314c2dd
Nusrat Jahan Nishat <nusnishat1999@gmail.com>
2024-07-15 20:02:45 +0600

    state declaration done

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { Children } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Root from './comonents/Root/Root.jsx';
+import Home from './comonents/Home/Home.jsx';
+import Donation from './comonents/Donation/Donation.jsx';
+import Statistics from './comonents/Statistics/Statistics.jsx';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    Children: [
+      {
+        path: "/home",
+        element: <Home></Home>
+      },
+      {
+        path: "/donation",
+        element: <Donation></Donation>
+      },
+      {
+        path: "/statistics",
+        element: <Statistics></Statistics>
+      },
+    ]
+  },
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
